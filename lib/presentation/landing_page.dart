3f9f88b962aa7100d6b256e8f598d3308dd09c46
rajeshsundar31 <rajeshsundarjr10@gmail.com>
2024-08-15 13:17:09 +0530

    landing page done

--- lib/presentation/landing_page.dart
+++ lib/presentation/landing_page.dart
@@ -1,4 +1,8 @@
+import 'package:finkin/components/custom_button.dart';
+import 'package:finkin/core/constants/app_color.dart';
+import 'package:finkin/core/constants/app_strings.dart';
 import 'package:finkin/core/utils/assetutils.dart';
+import 'package:finkin/core/utils/size.dart';
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
@@ -12,12 +16,28 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          const SizedBox(
-            height: 120,
+          const Padding(
+            padding: EdgeInsets.only(top: 90),
+            child: Text(AppStrings.welcome, style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 24
+            ),),
           ),
-          Image.asset(AssetsUtils.landingAvatar)
+          // SizedBox(
+          //   height: displayHeight(context)/9,
+          // ),
+          Image.asset(AssetsUtils.landingAvatar),
+
+          CustomButton(
+            color: AppColor.purple,
+            onPressed: (){},
+            width: displayWidth(context)/1.2,
+            text: AppStrings.continueBtn,
+          )
         ],
       ),
     );
